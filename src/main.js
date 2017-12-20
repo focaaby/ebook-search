51c12760591022bc2d47db4dddb3b34a5beff355
Jerry Wang <focaaby@gmail.com>
2017-12-20 15:49:55 +0800

    Added: axios as fetch package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import { Table, TableColumn, Input } from 'element-ui'
 import App from './App'
 
@@ -9,6 +11,7 @@ Vue.config.productionTip = false
 Vue.component(Table.name, Table)
 Vue.component(TableColumn.name, TableColumn)
 Vue.component(Input.name, Input)
+Vue.use(VueAxios, axios)
 
 /* eslint-disable no-new */
 new Vue({
